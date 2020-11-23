88106fd6fc2aa6f047f631c9e76a01c1c0003502
Florian <70654082+Florianfifteen@users.noreply.github.com>
2020-11-24 00:48:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,70 +1,95 @@
 <?php
- 
-$erreurs = "";
-$db = new PDO('mysql:host=localhost;dbname=todolist;charset=utf8', 'florian', 'glpi');
- 
-if (isset($_POST['creer_tache'])) { // On vérifie que la variable POST existe
-    if (empty($_POST['creer_tache'])) {  // On vérifie qu'elle as une valeure
-        $erreurs = 'Vous devez indiquer la valeure de la tâche';
-    } else {
-        $tache = $_POST['creer_tache'];
-        $db->exec("INSERT INTO tache(tache) VALUES('$taches')"); // On insère la tâche dans la base de donnée
-    }
+
+require("fonctions.inc.php");
+
+if (isset($session)) {
+
+  // Il s'agit d'une d�connexion, on supprime le sid
+  // -----------------------------------------------
+
+  mysql_connect($cfgHote, $cfgMajUsr, $cfgMajPwd);
+  $sql = "DELETE FROM sid WHERE sid='".$session."'";
+  $resultat = mysql_db_query($cfgBase, $sql);
+
 }
- 
-if(isset($_GET['supprimer_tache'])) {
-    $id = $_GET['supprimer_tache'];
-    $db->exec("DELETE FROM tache WHERE id=$id");
-}
- 
+
+entete("Gestion des T�ches", "", "focusPassword()");
+
 ?>
 
-<link rel="stylesheet" href="style.css"/>
- 
-<div class="header">
-    <p class="header_titre">Ma super Todo List ! </p>
-</div>
- 
- 
-<form class="taches_input" method="post" action="index.php">
- 
-    <input id="inserer" type="text" name="creer_tache"/>
-    <button id="envoyer">Créer</button>
+<!-- ---------------------------------------------- JS ------------------------------------------->
+<SCRIPT LANGUAGE="JavaScript">
 
-</form>
+function focusPassword() {
+  document.forms[0].ztPassword.focus();
+  document.forms[0].choix.value = "Se connecter";
+}
+
+function saisieOK() {
+
+  if (document.forms[0].zlInt.selectedIndex == 0) {
+    alert("Choisir l'utilisateur !");
+    return false;
+  } else
+    return true;
+}
+
+function Connexion(num) {
+
+  if (saisieOK()) {
+    if (num == 1)
+      document.forms[0].choix.value = "Se connecter";
+    else
+      document.forms[0].choix.value = "Changer de mot de passe";
+
+    document.forms[0].submit();
+  }
+
+}
+
+</SCRIPT>
+<!-- -------------------------------------------------- JS ------------------------>
 
 <?php
-if (isset($erreurs)) {
-    ?>
-    <p><?php echo $erreurs ?></p>
-    <?php
-}
+
+echo "<TABLE BGCOLOR='#FFFFFF' BORDER='0' CELLSPACING='0' CELLPADDING='1'><TR><TD>\n";
+echo "<TABLE BGCOLOR='#9DAEE8' BORDER='0' CELLSPACING='0' CELLPADDING='10'>\n";
+
+
+echo "<FORM METHOD=POST ACTION='valider-connexion.php' onSubmit='return saisieOK();'>";
+echo "<TR><TD VALIGN='center'>";
+
+echo "<BR>&nbsp;&nbsp;&nbsp;Utilisateur&nbsp;<BR>\n";
+echo "&nbsp;&nbsp;&nbsp;<SELECT NAME='zlInt' SIZE='1'>\n";
+
+// Affichage de la liste des utilisateurs
+// --------------------------------------
+$listeLimitee = 1;
+
+// On r�cup�re le cookie : code utilisateur
+if (isset($ckUtilisateur)) $zlInt = $ckUtilisateur;
+
+include("liste-intervenants.php");
+echo "</SELECT>&nbsp;&nbsp;<BR><BR>\n";
+
+echo "&nbsp;&nbsp;&nbsp;Mot de passe&nbsp;<BR>\n";
+echo "&nbsp;&nbsp;&nbsp;<INPUT TYPE='password' NAME='ztPassword' MAXLENGTH='8'>&nbsp;<BR><BR>\n";
+
+echo "</TD></TR></TABLE>\n";
+echo "</TD></TR></TABLE>\n";
+
+?>		
+
+<BR><INPUT TYPE="hidden" NAME="choix" VALUE="Se connecter">
+&nbsp;&nbsp;<IMG SRC='images/droite.gif' ALIGN='absmiddle' BORDER='0'>&nbsp;<A HREF="javascript:Connexion(1)" CLASS="Liens">Se connecter</A><BR><BR>
+
+<?php
+
+echo "&nbsp;&nbsp;<IMG SRC='images/droite.gif' ALIGN='absmiddle' BORDER='0'>&nbsp;<A HREF='javascript:Connexion(2)' CLASS='Liens'>Changer de mot de passe</A>\n";
+
 ?>
 
-<table class="taches">
-    <tr>
-        <th>
-            N
-        </th>
-        <th>
-            Nom
-        </th>
-        <th>
-            Action
-        </th>
-    </tr>
-    <?php
-    $reponse = $db->query('Select * from tache'); // On exécute une requête visant à récupérer les tâches
-    while ($taches = $reponse->fetch()){ // On initialise une boucle
-        ?>
-        <tr>
-            <td><?php echo $taches['id'] ?></td>
-            <td><?php echo $taches['tache'] ?></td>
-            <td><a class="suppr" href="index.php?supprimer_tache=<?php echo $taches['id'] ?>"> X</a></td>
-        </tr>
-        <?php
-    }
- 
-    ?>
- 
-</table>
+</FORM>
+
+</BODY>
+</HTML>
